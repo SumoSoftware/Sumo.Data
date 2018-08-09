5a6b34053d522f98968f3d3af9745d32edeb3b8c
Mark Lauter <ml.github@sumosoftware.com>
2018-08-08 20:07:15 -0400

    replaced Sumo.Retry.Example

---
+++ Sumo.Retry.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sumo.Retry.Example
+{
+    class Program
+    {
+        //todo: pull retry sample code from Sumo.Retry and put it here
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
