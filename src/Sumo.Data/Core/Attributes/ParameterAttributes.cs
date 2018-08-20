9ae5002bdfbabae14abcab4347a5ef9bfb918483
Mark Lauter <ml.github@sumosoftware.com>
2018-08-20 10:36:15 -0400

    fixed visibility of static type info cache classes

--- src/Sumo.Data/Core/Attributes/ParameterAttributes.cs
+++ src/Sumo.Data/Core/Attributes/ParameterAttributes.cs
@@ -22,80 +22,6 @@ namespace Sumo.Data
             ParameterSize = parameterSize;
         }
 
-        //public ParameterSizeAttribute(SqlDbType type)
-        //{
-        //    throw new NotImplementedException();
-
-        //    //todo: set size based on SqlDbType
-        //    //switch (type)
-        //    //{
-        //    //    case SqlDbType.BigInt:
-        //    //        break;
-        //    //    case SqlDbType.Binary:
-        //    //        break;
-        //    //    case SqlDbType.Bit:
-        //    //        break;
-        //    //    case SqlDbType.Char:
-        //    //        break;
-        //    //    case SqlDbType.Date:
-        //    //        break;
-        //    //    case SqlDbType.DateTime:
-        //    //        break;
-        //    //    case SqlDbType.DateTime2:
-        //    //        break;
-        //    //    case SqlDbType.DateTimeOffset:
-        //    //        break;
-        //    //    case SqlDbType.Decimal:
-        //    //        break;
-        //    //    case SqlDbType.Float:
-        //    //        break;
-        //    //    case SqlDbType.Image:
-        //    //        break;
-        //    //    case SqlDbType.Int:
-        //    //        break;
-        //    //    case SqlDbType.Money:
-        //    //        break;
-        //    //    case SqlDbType.NChar:
-        //    //        break;
-        //    //    case SqlDbType.NText:
-        //    //        break;
-        //    //    case SqlDbType.NVarChar:
-        //    //        break;
-        //    //    case SqlDbType.Real:
-        //    //        break;
-        //    //    case SqlDbType.SmallDateTime:
-        //    //        break;
-        //    //    case SqlDbType.SmallInt:
-        //    //        break;
-        //    //    case SqlDbType.SmallMoney:
-        //    //        break;
-        //    //    case SqlDbType.Structured:
-        //    //        break;
-        //    //    case SqlDbType.Text:
-        //    //        break;
-        //    //    case SqlDbType.Time:
-        //    //        break;
-        //    //    case SqlDbType.Timestamp:
-        //    //        break;
-        //    //    case SqlDbType.TinyInt:
-        //    //        break;
-        //    //    case SqlDbType.Udt:
-        //    //        break;
-        //    //    case SqlDbType.UniqueIdentifier:
-        //    //        break;
-        //    //    case SqlDbType.VarBinary:
-        //    //        break;
-        //    //    case SqlDbType.VarChar:
-        //    //        break;
-        //    //    case SqlDbType.Variant:
-        //    //        break;
-        //    //    case SqlDbType.Xml:
-        //    //        break;
-        //    //    default:
-        //    //        break;
-        //    //}
-        //}
-
         public int ParameterSize { get; } = -1;
     }
 
@@ -105,8 +31,6 @@ namespace Sumo.Data
         public InputOutputParameterAttribute() : base() { }
 
         public InputOutputParameterAttribute(int parameterSize) : base(parameterSize) { }
-
-        //public InputOutputParameterAttribute(SqlDbType type) : base(type) { }
     }
 
     [AttributeUsage(AttributeTargets.Property)]
@@ -115,7 +39,5 @@ namespace Sumo.Data
         public OutputParameterAttribute() : base() { }
 
         public OutputParameterAttribute(int parameterSize) : base(parameterSize) { }
-
-        //public OutputParameterAttribute(SqlDbType type) : base(type) { }
     }
 }
