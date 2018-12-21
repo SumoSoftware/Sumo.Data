ae60ecdbab386e391ca6ac5df74a6145d2d26500
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-21 11:16:00 -0500

    added local sql server database so the sql server tests can be made to be deterministic

---
+++ test/Sumo.Data.Test/Test.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Test]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR(256) NOT NULL,
+	[Status] int not null,
+	CONSTRAINT [TestPk] PRIMARY KEY CLUSTERED ([Id] ASC),
+)
