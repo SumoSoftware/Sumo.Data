f709e081e87b550d83b83a83401742d5a97ab287
Mark Lauter <mark.lauter@sumosoftware.com>
2019-03-23 15:01:42 -0400

    updated nuspecs to enable debug for local builds

--- scripts/PackNPush_Local_Debug.ps1
+++ scripts/PackNPush_Local_Debug.ps1
@@ -2,7 +2,7 @@
 
 # sumo local folder - debug
 Remove-Item *.nupkg
-nuget.exe pack -Build -Symbols -OutputDirectory "." -Verbosity quiet -properties Configuration=Debug
+nuget.exe pack -Build -Symbols -OutputDirectory "." -Verbosity quiet -properties Configuration=Debug -suffix debug
 nuget.exe push *.nupkg -Source "Local"
 
 Remove-Item *.nupkg
